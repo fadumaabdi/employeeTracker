5435299e9568783c3458582e52e7bcbfe51076b8
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-05 22:29:25 +0100

    roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,14 @@ INSERT INTO department(id,name) VALUES (1, "Engineering");
 INSERT INTO department(id,name) VALUES (1, "Finance");
 INSERT INTO department(id,name) VALUES (1, "Legal");
 
+INSERT INTO role(id,title,salary,departmentID) VALUES (1, "Sales Lead", 100000, 1);
+INSERT INTO role(id,title,salary,departmentID) VALUES (2, "Sales Person", 80000, 1);
+INSERT INTO role(id,title,salary,departmentID) VALUES (3, "Lead Engineer", 200000, 2);
+INSERT INTO role(id,title,salary,departmentID) VALUES (4, "Software Engineer", 180000, 2);
+INSERT INTO role(id,title,salary,departmentID) VALUES (5, "Accountant", 150000, 3);
+INSERT INTO role(id,title,salary,departmentID) VALUES (6, "Legal Team Lead", 200000, 4);
+INSERT INTO role(id,title,salary,departmentID) VALUES (7, "Lawyer", 100000, 4);
+
+
+
+
