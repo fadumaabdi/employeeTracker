24efc00892b72ae097f846e6f7be961d995c398f
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-05 22:26:18 +0100

    DEPARTMENTS

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department(id,name) VALUES (1, "Sales");
+INSERT INTO department(id,name) VALUES (1, "Engineering");
+INSERT INTO department(id,name) VALUES (1, "Finance");
+INSERT INTO department(id,name) VALUES (1, "Legal");
+
