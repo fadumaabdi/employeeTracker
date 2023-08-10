76d368639eed8ab85d92a161214f1a52f38a9dc1
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-11 00:32:19 +0100

    constructoroptions corrected

--- db/database.js
+++ db/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2')
 
 class database{
-    constructor (option) {
-        this.options = this.options
+    constructor (options) {
+        this.options = options;
         this.db = null
     }
 
